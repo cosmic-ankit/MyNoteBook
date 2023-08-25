61e8c256c00d63491e75097dee689b74e0a41fdc
Ankit Jain <ankit9252984512@gmail.com>
2023-08-25 16:13:59 +0530

    Updating the mongoose .connect function

--- Backend/db.js
+++ Backend/db.js
@@ -2,13 +2,24 @@ const mongoose = require('mongoose'); // Importing mongoose
 
 const mongoURI = "mongodb://localhost:27017/"  // Its a variable that holds the value of mongodb string
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfully");
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
-}
+    .then(() => {
+        console.log("Connected to MongoDB successfully");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+};
 
-//This is a function where we have call the mongoose.connect which helps in connecting the database and when the connection is established the callback function of connected to mong will be called
+//This is a promise function where we have call the mongoose.connect which helps in connecting the database and when the connection is established the connected to mong will be printed else the error will be logged
+
+// I've added options (useNewUrlParser and useUnifiedTopgy) to the mongoose.connect() method. These options are required for the latest version of Mongoose and the MongoDB driver. useNewUrlParser helps with parsing connection strings, and useUnifiedTopology enables the new topology engine.
+
+
 
 module.exports = connectToMongo;
 
